8ba7088f23bbdc704a2efd02c905cad98c2dc2a6
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-12 16:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-//complete this code
 class Person {
-	constructor(name,age){
-	this.name=name;
-	this.age=age;
+  constructor(name, age) {
+    this.name = name; // Use public field syntax
+    this.age = age; // Use public field syntax
+  }
+
+  // No need for getter and setter
+
 }
-	// get name(){
-	// 	return this.name;
-	// }
-	// set age(age){
-	// 	return this.age
-	// }
-}
+
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
